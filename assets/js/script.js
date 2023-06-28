6f2abf0a8572aa78114cf6641e5c3e5ad0e1e28f
Deivissonxavier <deivissonx3@gmail.com>
2023-06-27 22:30:31 -0300

    atualizando

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,21 @@
 
 /*Alterar o tema */
-
 const icon = document.querySelector('.icon');
 const body = document.body;
 
 icon.addEventListener('click', function () {
   body.classList.toggle('dark-mode');
-  
+
   if (body.classList.contains('dark-mode')) {
     icon.src = 'assets/img/icon-dark.png';
   } else {
     icon.src = 'assets/img/icon-light.png';
   }
 });
+
+
+/* Animações */
+
+window.sr = ScrollReveal({ reset: true });
+sr.reveal('.topic, h2, .imagem, p, .video, footer', { duration: 1500 });
+
